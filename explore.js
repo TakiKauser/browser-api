87b84a8cdc7d5d53aad2307482067233da74e744
TakiKauser <takikauserbadhon@gmail.com>
2021-09-05 00:00:39 +0600

    dom explored

---
+++ explore.js
@@ -0,0 +1,14 @@
+const name = 15;
+
+const student = {
+    name: "kb",
+    friend: "fft",
+    job: "dev",
+    problemSolved: function(OJ){
+        console.log(OJ);
+    }
+};
+student.problemSolved("CodeForces");
+
+// Document Object Model as it behaves like objects
+// document.getElementById("name");
